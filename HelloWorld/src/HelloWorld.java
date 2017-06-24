f173ee1882fd5915835d828d52f843621a8e3cb3
solomonrajdd <kingsolo@rediffmail.com>
2017-06-24 12:46:27 +0530

    commiting HelloWorld 1st JavaPg to branch

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,11 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello, Welcome to the World of Java Programming");
+		System.out.println("\n Have Fun while Programming Java");
+	}
+
+}
